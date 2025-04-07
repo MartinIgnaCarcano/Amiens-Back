94a9028d32d45cdad928edca65caf9efc8e036a0
MartinIgnaCarcano <149337574+MartinIgnaCarcano@users.noreply.github.com>
2025-04-06 22:42:47 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS  # Importa el módulo
+from flask_cors import CORS
 import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # Habilita CORS para la app
-    # Configuración de la base de datos
-    
-     # Asegurar carpeta persistente en Render
-    db_path = '/data/inventario.db'
-    os.makedirs('/data', exist_ok=True)
+    CORS(app)
 
-    # Configuración de la base de datos SQLite persistente
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/inventario.db'
+    # Usar la carpeta persistente que Render te asigna
+    persist_path = os.environ.get('RENDER_PERSISTENT_FS_PATH', './')
+    db_path = os.path.join(persist_path, 'inventario.db')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     db.init_app(app)
-    
-    # Importar rutas después de crear la app para evitar circular imports
+
     from .routes import productos_bp
     app.register_blueprint(productos_bp)
-    
+
     with app.app_context():
         db.create_all()
-    
+
     return app
