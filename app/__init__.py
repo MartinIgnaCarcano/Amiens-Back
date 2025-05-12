9730b578ac64cfff8e8450c2dced6d28b64e995b
MartinPc <martin.carcano342@gmail.com>
2025-05-12 18:05:34 -0300

    Primer Commit

---
+++ app/__init__.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from flask_jwt_extended import JWTManager
+
+# Inicializar SQLAlchemy sin asociar aún con la app
+db = SQLAlchemy()
+
+# Cargar variables de entorno desde .env
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configurar base de datos (usa la URL real de Supabase en tu .env)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        # Configurar base de datos (usa la URL real de Supabase en tu .env)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres.uvqhfytlzlxwnjhxulfg:rinkashichikito@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    CORS(app)
+    db.init_app(app)
+
+    with app.app_context():
+        from app import models
+        from .routes import productos_bp, auth_bp
+        app.register_blueprint(productos_bp)
+        app.register_blueprint(auth_bp)
+        
+        
+    return app
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import text
+
+# # Crear la instancia de la base de datos
+# db = SQLAlchemy()
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     # Configuración de la base de datos (sin .env)
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres.uvqhfytlzlxwnjhxulfg:rinkashichikito@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#     # Inicializar la base de datos con la app
+#     db.init_app(app)
+
+#     # Ruta para verificar que la conexión funciona
+#     @app.route('/ping_db')
+#     def ping_db():
+#         try:
+#             # Ejecutar un SELECT 1 usando text()
+#             result = db.session.execute(text('SELECT 1')).scalar()
+#             return f"Conexión exitosa. Resultado: {result}"
+#         except Exception as e:
+#             return f"Error al conectar a la base de datos: {e}"
+
+#     return app
